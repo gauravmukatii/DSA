a7b2998cc20ec3be8926e22fa9cd5c1640b194f2
Gaurav Mukati <gaurav.mukati99@gmail.com>
2023-12-20 15:44:52 +0530

    dsa

--- DSA/SubSq.java
+++ DSA/SubSq.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
 
 public class SubSq {
     public static void main(String[] args) {
-        subset("", "abcd");
+        ArrayList<String> list = subset1("", "abc");
+        for (String ss : list) {
+            System.out.println(ss);
+        }
+
     }
 
     public static void subset(String p, String up) {
@@ -14,5 +19,21 @@ public class SubSq {
 
         subset(p + ch, up.substring(1));
         subset(p, up.substring(1));
+    }
+
+    public static ArrayList<String> subset1(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subset1(p + ch, up.substring(1));
+        ArrayList<String> right = subset1(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
